a97c61671bf9f4a07d0ac30f822340c20dc8eb82
Shinya Takeuchi <tak6uch1@gmail.com>
2022-10-17 09:44:25 +0900

    Update build script and add config.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-TAGVERNUM=`uname -r -v | sed 's/\-.*//'`
+#TAGVERNUM=`uname -r -v | sed 's/\-.*//'`
+TAGVERNUM=5.15.57.1
 TAGVER=linux-msft-wsl-${TAGVERNUM}
 echo "TAGVERNUM:$TAGVERNUM"
 echo "TAGVER:$TAGVER"
 
-USER=tak6u
+USER=`whoami`
 nproc=5
 
 rundir=$PWD
@@ -19,19 +20,21 @@ sudo apt install -y build-essential flex bison \
 
 # Download source code of WLS2 kernel
 cd /usr/src
-sudo git clone -b ${TAGVER} https://github.com/microsoft/WSL2-Linux-Kernel.git ${TAGVERNUM}-microsoft-standard
-cd ${TAGVERNUM}-microsoft-standard
+#sudo git clone -b ${TAGVER} https://github.com/microsoft/WSL2-Linux-Kernel.git ${TAGVERNUM}-microsoft-standard
+#cd ${TAGVERNUM}-microsoft-standard
+sudo git clone https://github.com/microsoft/WSL2-Linux-Kernel.git -b ${TAGVER} --depth 1
+cd WSL2-Linux-Kernel
 
 # Copy config file
-# Get PINTO's config already set by menuconfig
-sudo wget -O .config https://github.com/PINTO0309/wsl2_linux_kernel_usbcam_enable_conf/raw/main/${TAGVER}/config
+sudo cp $rundir/config .config
 sudo chmod 777 .config
 # If you want to change config setting, execute below.
 #sudo make menuconfig
 
 # Build kernel
 sudo make clean
-sudo make -j $nproc && sudo make modules_install -j $nproc $$ sudo make install -j $nproc
+#sudo make -j $nproc KCONFIG_CONFIG=.config && sudo make modules_install -j $nproc $$ sudo make install -j $nproc
+sudo make -j$(nproc) && sudo make modules_install -j$(nproc) && sudo make install -j$(nproc)
 
 # Build USBIP
 cd tools/usb/usbip
@@ -46,7 +49,8 @@ sudo make install -j $nproc
 sudo cp libsrc/.libs/libusbip.so.0 /lib/libusbip.so.0
 
 # Copy kernel into user's home directory
-sudo cp /usr/src/${TAGVERNUM}-microsoft-standard/vmlinux /mnt/c/Users/$USER/
+#sudo cp /usr/src/${TAGVERNUM}-microsoft-standard/vmlinux.o /mnt/c/Users/$USER/vmlinux
+sudo cp /usr/src/WSL2-Linux-Kernel/vmlinux /mnt/c/Users/$USER/vmlinux
 
 # Make .wslconfig
 cat << EOF > /mnt/c/Users/$USER/.wslconfig
